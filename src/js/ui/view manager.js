5a003dfc5f571d9b98b9cbf4beed3c08e2d82d93
Sleepy-Reacon <63058519+Sleepy-Reacon@users.noreply.github.com>
2021-01-15 21:26:12 +0100

    Archive

---
+++ src/js/ui/view manager.js
@@ -0,0 +1,68 @@
+window.addEventListener("load", () => {
+    document.getElementById("navLink1").addEventListener("click", () => {
+        document.getElementById("navLink1").style.display = "none";
+        document.getElementById("navLink2").style.display = "";
+        clearView({
+            mainView: true
+        });
+    });
+
+    document.getElementById("navLink2").addEventListener("click", () => {
+        document.getElementById("navLink1").style.display = "";
+        document.getElementById("navLink2").style.display = "none";
+        clearView({
+            settingsView: true
+        });
+    });
+
+    document.getElementById("toolLink1").addEventListener("click", () => {
+        document.getElementById("navLink1").style.display = "";
+        document.getElementById("navLink2").style.display = "none";
+        clearView({
+           fileView: true
+        });
+    });
+
+    document.getElementById("toolLink2").addEventListener("click", () => {
+        document.getElementById("navLink1").style.display = "";
+        document.getElementById("navLink2").style.display = "none";
+        clearView({
+            folderView: true
+        });
+    });
+
+    document.getElementById("navLink1").style.display = "none";
+    clearView({
+        mainView: true
+    });
+});
+
+function clearView({
+                       mainView = false,
+                       fileView = false,
+                       folderView = false,
+                       settingsView = false
+                   }) {
+    let main = document.getElementById("mainView");
+    let file = document.getElementById("addFileView");
+    let folder = document.getElementById("addFolderView");
+    let settings = document.getElementById("settingsView");
+
+    main.style.display = "none";
+    file.style.display = "none";
+    folder.style.display = "none";
+    settings.style.display = "none";
+
+    if (mainView) {
+        main.style.display = "";
+    }
+    if (fileView) {
+        file.style.display = "";
+    }
+    if (folderView) {
+        folder.style.display = "";
+    }
+    if (settingsView) {
+        settings.style.display = "";
+    }
+}
